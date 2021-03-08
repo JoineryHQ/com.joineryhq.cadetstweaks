682a83dd95539517444fcc678c5a8c4a2c56f3bd
Allen Shaw <allen@JoineryHQ.com>
2021-03-08 13:30:12 -0600

    Bug-fix: fatal SQL error "already exists" for temp table when processing multiple contacts in series.

--- CRM/Cadetstweaks/Utils.php
+++ CRM/Cadetstweaks/Utils.php
@@ -4,10 +4,28 @@ use CRM_Cadetstweaks_ExtensionUtil as E;
 class CRM_Cadetstweaks_Utils {
 
   /**
+   * Process contacts (either the given contact or all contacts) by updating the
+   * custom "age at cut-off" field based on contact birth_date.
+   *
    * @param $cid int contact id
    *
+   * @return array of query result per CRM_Core_DAO::executeQuery().
    */
-  public static function buildUpdatesTable($cid = NULL) {
+  public static function runUpdateCutoffAges($cid = NULL) {
+    // Get the custom field to get the custom group value column
+    $getAgeCutOffField = \Civi\Api4\CustomField::get()
+      ->addWhere('name', '=', 'Age_at_cut_off')
+      ->setCheckPermissions(FALSE)
+      ->execute()
+      ->first();
+
+    // Get the custom group to get the custom group value table
+    $getCadetsExtra = \Civi\Api4\CustomGroup::get()
+      ->addWhere('title', '=', 'Cadets Extra')
+      ->setCheckPermissions(FALSE)
+      ->execute()
+      ->first();
+
     // Create temporary table to store contact id and age cutoff
     $query = "CREATE TEMPORARY TABLE `CRM_Cadetstweaks_Utils_buildUpdatesTable`
       (PRIMARY KEY id (id))
@@ -39,29 +57,6 @@ class CRM_Cadetstweaks_Utils {
 
     // Execute query
     CRM_Core_DAO::executeQuery($query, $queryParams);
-  }
-
-  /**
-   * @param $cid int contact id
-   *
-   * @return array of query result
-   */
-  public static function runUpdateCutoffAges($cid = NULL) {
-    // Get the custom field to get the custom group value column
-    $getAgeCutOffField = \Civi\Api4\CustomField::get()
-      ->addWhere('name', '=', 'Age_at_cut_off')
-      ->setCheckPermissions(FALSE)
-      ->execute()
-      ->first();
-
-    // Get the custom group to get the custom group value table
-    $getCadetsExtra = \Civi\Api4\CustomGroup::get()
-      ->addWhere('title', '=', 'Cadets Extra')
-      ->setCheckPermissions(FALSE)
-      ->execute()
-      ->first();
-
-    self::buildUpdatesTable($cid);
 
     // Insert/Update cutoff age of every contact using the custom group value table and column
     $query = "INSERT INTO `{$getCadetsExtra['table_name']}` (entity_id, {$getAgeCutOffField['column_name']})
@@ -73,6 +68,13 @@ class CRM_Cadetstweaks_Utils {
     // Execute query
     $result = CRM_Core_DAO::executeQuery($query);
 
+    // Drop the table. Yes, it's tempoarary, but if we process multiple separate
+    // contacts in the same PHP call, that temporary table will still exist for the
+    // next contact, leading to fatal SQL "already exists" error. Therefore we have
+    // to drop it now.
+    CRM_Core_DAO::executeQuery("DROP TABLE IF EXISTS `CRM_Cadetstweaks_Utils_buildUpdatesTable`");
+
     return $result;
   }
+
 }
