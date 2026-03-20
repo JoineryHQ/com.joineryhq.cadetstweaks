8cd97aecd85d896f0879e0eee4d2fde98fa0e2ac
Allen Shaw <allen@JoineryHQ.com>
2026-03-20 18:21:20 -0500

    civix upgrade, format: 25.10.2

--- cadetstweaks.php
+++ cadetstweaks.php
@@ -31,39 +31,12 @@ function cadetstweaks_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function cadetstweaks_civicrm_xmlMenu(&$files) {
-  _cadetstweaks_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
  */
 function cadetstweaks_civicrm_install() {
   _cadetstweaks_civix_civicrm_install();
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function cadetstweaks_civicrm_postInstall() {
-  _cadetstweaks_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function cadetstweaks_civicrm_uninstall() {
-  _cadetstweaks_civix_civicrm_uninstall();
 }
 
 /**
@@ -178,90 +151,6 @@ function cadetstweaks_civicrm_searchColumns($objectName, &$headers, &$rows, &$se
  */
 function cadetstweaks_civicrm_enable() {
   _cadetstweaks_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function cadetstweaks_civicrm_disable() {
-  _cadetstweaks_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function cadetstweaks_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _cadetstweaks_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function cadetstweaks_civicrm_managed(&$entities) {
-  _cadetstweaks_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function cadetstweaks_civicrm_caseTypes(&$caseTypes) {
-  _cadetstweaks_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function cadetstweaks_civicrm_angularModules(&$angularModules) {
-  _cadetstweaks_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function cadetstweaks_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _cadetstweaks_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function cadetstweaks_civicrm_entityTypes(&$entityTypes) {
-  _cadetstweaks_civix_civicrm_entityTypes($entityTypes);
-}
-
-/**
- * Implements hook_civicrm_thems().
- */
-function cadetstweaks_civicrm_themes(&$themes) {
-  _cadetstweaks_civix_civicrm_themes($themes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
